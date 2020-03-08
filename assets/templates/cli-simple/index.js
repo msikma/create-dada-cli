f2e5459bb653483c56401d1de99a118eede00948
Michiel Sikma <michiel@sikma.org>
2020-03-08 17:56:46 +0100

    Add example argparse without logger to the simple template

--- assets/templates/cli-simple/index.js
+++ assets/templates/cli-simple/index.js
@@ -2,3 +2,35 @@
 
 // {{name}}-cli <{{homepage}}>
 // © {{license}} license
+
+const { resolve } = require('path')
+const { makeArgParser } = require('dada-cli-tools/argparse')
+const { ensurePeriod } = require('dada-cli-tools/util/text')
+const { readJSONSync } = require('dada-cli-tools/util/fs')
+
+const pkgPath = resolve(__dirname)
+const pkgData = readJSONSync(`${pkgPath}/package.json`)
+
+const parser = makeArgParser({
+  version: pkgData.version,
+  addHelp: true,
+  longHelp: (
+    `Example long help.`
+  ),
+  description: ensurePeriod(pkgData.description),
+  epilog: 'Example epilog.'
+})
+
+parser.addArgument('--arg', { help: 'Example argument.', metavar: 'PATH', dest: 'exampleArgument', defaultValue: `hello` })
+parser.addArgument('--arg-two', { help: 'Example boolean argument.', action: 'storeTrue', dest: 'exampleTwo' })
+
+// Parse input. If usage is incorrect, the program will exit and display an error here.
+const parsed = { ...parser.parseArgs() }
+
+/** Main program. */
+const main = (args, { pkgData, baseDir }) => {
+  // main program
+}
+
+// Run the main program.
+main(parsed, { pkgData, baseDir: pkgPath })
